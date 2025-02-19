20a27c45e34c2fba1b2c3ae5232b93364b0d6e14
Katsuki <teeangly168@gmail.com>
2025-02-19 19:16:19 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import StorePage from "./pages/StorePage";
 import OrderPage from "./pages/OrderPage";
 import InboxPage from "./pages/InboxPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<StorePage />} />
-        <Route path="/order" element={<OrderPage />} />
-        <Route path="/inbox" element={<InboxPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<StorePage />} />
+      <Route path="/order" element={<OrderPage />} />
+      <Route path="/inbox" element={<InboxPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
