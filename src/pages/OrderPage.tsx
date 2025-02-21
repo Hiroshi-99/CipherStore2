6b8e70720cbd457fb2f350521191a39725db851a
Katsuki <teeangly168@gmail.com>
2025-02-21 23:52:05 +0700

    update

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -335,8 +335,7 @@ function OrderPage() {
       <div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage:
-            'url("https://cdn.discordapp.com/attachments/1335202613913849857/1341847795807813815/wallpaperflare.com_wallpaper.jpg?ex=67b77ca4&is=67b62b24&hm=17f869720e0d7d178e5a1d6140243b37f248c32e837142aded205cd3c4453de1&")',
+          backgroundImage: 'url("https://i.imgur.com/crS3FrR.jpeg")',
           backgroundSize: "cover",
           backgroundPosition: "center",
           filter: "brightness(0.7)",
